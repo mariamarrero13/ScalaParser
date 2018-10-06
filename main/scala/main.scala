0b4bf72794b59742a4e9e4f051cb61fa074f85e0
Maria Marrero <maria.marrero13@upr.edu>
2018-10-06 17:25:20 -0400

    tried to correct errors, no excetions are ocurring

--- main/scala/main.scala
+++ main/scala/main.scala
@@ -1,3 +1,13 @@
-import CICOMparser
+import scala.io._
 
-def main()
+object console {
+    
+  def main(args: Array[String]): Unit = {
+        val parsers = new CICOMparser
+        var strToParse = "let\n  f := map n to if n = 0 then 1 else n * f(n - 1);\nin\n  let\n    f := map n,m,k to if (n <= 0 & n >= 0)\n                  | (n < 0 & n > 0 & n != 0) then number?\n                                           else m / f(k + 1);\n  in\n     let x:=3;\n         y:=4;\n         z:=cons?(function?(x * ~y), cons(-arity(x)));\n     in\n        let x:=3;\n            y:=4;\n            z:=g();\n        in\n            (g(x,y,z))(null?(true),list?(false),first(null))"
+        val parser = new CICOMparser
+        val result = parser.parseAll(parser.Exp, strToParse)
+        println(result.toString)
+  }
+  
+}
--- main/scala/parser.scala
+++ main/scala/parser.scala
@@ -6,20 +6,20 @@ class CICOMparser extends RegexParsers{
     val delimiter: Parser[Any] = "[()\\[\\];,]".r
     val operator: Parser[Any] = "[\\+-~*/=(!=)<>(<=)(>=)&|(:=)]".r
 
-    def Exp : Parser[Any] = "if"~Exp ~"then" ~ Exp ~ "else" ~ Exp | "let"~ rep1(Def)~"in"~ Exp |Term~opt(Binop~Exp)
-    def Term : Parser[Any] = Unop~Term | Factor ~ opt(ExpList)| Empty | int | Bool 
-    def Factor : Parser[Any] = Exp | Prim | Id
-    def ExpList : Parser[Any] = PropExpList
-    def PropExpList : Parser[Any] = Exp | Exp~","~PropExpList
-    def IdList : Parser[Any] = PropIdList
-    def PropIdList : Parser[Any] = Id | Id~","~ PropIdList
-    def Def : Parser[Any] = Id ~ ":=" ~ Exp
-    def Empty : Parser[Any] = ""
+    def Exp : Parser[Any] = "if"~Exp ~"then" ~ Exp ~ "else" ~ Exp | "let"~ rep1(Def)~"in"~ Exp |"map" ~ IdList ~ "to" ~ Exp|Term~rep(Binop~Exp)
+    def Term : Parser[Any] = Unop~Term | Factor ~ rep( "(" ~ExpList~ ")")| Empty | int | Bool 
+    def Factor : Parser[Any] = "(" ~ Exp ~ ")" | Prim | Id
+    def ExpList : Parser[Any] = rep(PropExpList)
+    def PropExpList : Parser[Any] = Exp~","~PropExpList|Exp 
+    def IdList : Parser[Any] = rep(PropIdList)
+    def PropIdList : Parser[Any] = Id~","~ PropIdList |Id 
+    def Def : Parser[Any] = Id ~ ":=" ~ Exp ~ ";"
+    def Empty : Parser[Any] = "empty"
     def Bool : Parser[Any] = "true" | "false"
     def Unop : Parser[Any] = Sign | "~"
     def Sign : Parser[Any] = "+" | "-"
-    def Binop : Parser[Any] = Sign | "*" | "/" | "=" | "!=" | "<" | ">" | "<=" | ">=" | "&" | "|"
+    def Binop : Parser[Any] = Sign | "*" | "/" | "=" | "!=" |  "<=" | ">=" |"<" | ">" | "&" | "|"
     def Prim : Parser[Any] = "number?" | "function?" | "list?" |"empty?" | "cons?" | "cons" | "first" | "rest" | "arity"
-    def Id : Parser[Any] = character ~ opt(character|digit)
-    def int : Parser[Any] = digit+
+    def Id : Parser[Any] = character ~ rep((character|digit)~"*")
+    def int : Parser[Any] = rep1(digit)
 }

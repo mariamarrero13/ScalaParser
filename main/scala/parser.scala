9bd1dedb8238716048808c680954a87af67c4f4e
Maria Marrero <maria.marrero13@upr.edu>
2018-10-06 12:36:19 -0400

    Made corrections

--- main/scala/parser.scala
+++ main/scala/parser.scala
@@ -1,26 +1,25 @@
 import scala.util.parsing.combinator._
 
-class Parser extends RegexParsers{
+class CICOMparser extends RegexParsers{
     val digit : Parser[Any] = "[0-9]".r
     val character: Parser[Any] = "[A-za-z_?]".r
     val delimiter: Parser[Any] = "[()\\[\\];,]".r
     val operator: Parser[Any] = "[\\+-~*/=(!=)<>(<=)(>=)&|(:=)]".r
 
-    def Exp : Parser[Any] = Term~opt(Binop~exp)|"if" ~ Exp ~ "then" ~ Exp ~ "else" Exp | "let" ~ Def+ ~ "in" ~ Exp
+    def Exp : Parser[Any] = "if"~Exp ~"then" ~ Exp ~ "else" ~ Exp | "let"~ rep1(Def)~"in"~ Exp |Term~opt(Binop~Exp)
     def Term : Parser[Any] = Unop~Term | Factor ~ opt(ExpList)| Empty | int | Bool 
     def Factor : Parser[Any] = Exp | Prim | Id
     def ExpList : Parser[Any] = PropExpList
     def PropExpList : Parser[Any] = Exp | Exp~","~PropExpList
     def IdList : Parser[Any] = PropIdList
     def PropIdList : Parser[Any] = Id | Id~","~ PropIdList
-    def Def : Parser[Any] = Id = Exp
+    def Def : Parser[Any] = Id ~ ":=" ~ Exp
     def Empty : Parser[Any] = ""
     def Bool : Parser[Any] = true | false
     def Unop : Parser[Any] = Sign | "~"
-    def Sign : Parser[Any] = "+"|"-"
+    def Sign : Parser[Any] = "+" | "-"
     def Binop : Parser[Any] = Sign | "*" | "/" | "=" | "!=" | "<" | ">" | "<=" | ">=" | "&" | "|"
     def Prim : Parser[Any] = "number?" | "function?" | "list?" |"empty?" | "cons?" | "cons" | "first" | "rest" | 
     def Id : Parser[Any] = character ~ opt(Character|Digit)*
     def int : Parser[Any] = digit+
-
 }

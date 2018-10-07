e50cf4803f3ac60e463cd214bcda6177df62c968
Maria Marrero <maria.marrero13@upr.edu>
2018-10-07 18:48:08 -0400

    added reserved words support. Parser works.

--- main/scala/main.scala
+++ main/scala/main.scala
@@ -5,7 +5,6 @@ object console {
       def main(args: Array[String]): Unit = {
        // var strToParse = "let\n  f := map n to if n = 0 then 1 else n * f(n - 1);\nin\n  let\n    f := map n,m,k to if (n <= 0 & n >= 0)\n                  | (n < 0 & n > 0 & n != 0) then number?\n                                           else m / f(k + 1);\n  in\n     let x:=3;\n         y:=4;\n         z:=cons?(function?(x * ~y), cons(-arity(x)));\n     in\n        let x:=3;\n            y:=4;\n            z:=g();\n        in\n            (g(x,y,z))(null?(true),list?(false),first(null))"
         var strToParse = scala.io.Source.fromFile("testprogram.txt").mkString
-        println(strToParse)
         val parser = new CICOMparser
         val result = parser.parseAll(parser.Exp, strToParse)
         println(result.toString)
--- main/scala/parser.scala
+++ main/scala/parser.scala
@@ -3,7 +3,7 @@ import scala.util.parsing.combinator._
 class CICOMparser extends RegexParsers{
     def Digits : Parser[Any] = "[0-9]".r
     def Characters: Parser[Any] = "[A-za-z_?]".r
-    def Exp : Parser[Any] =  "let"~ Def~rep(Def)~"in"~ Exp |"map"~ IdList~"to" ~ Exp|"if"~Exp~"then"~Exp~"else"~Exp |Term~rep(Binop~Exp)
+    def Exp : Parser[Any] =  "let"~ rep1(Def)~"in"~ Exp |"map"~ IdList~"to" ~ Exp|"if"~Exp~"then"~Exp~"else"~Exp |Term~rep(Binop~Exp)
     def Term : Parser[Any] =  Empty | Intg | Bool |Factor ~ rep( "(" ~ExpList~ ")")|Unop~Term 
     def Factor : Parser[Any] = "(" ~ Exp ~ ")" | Prim | Id
     def ExpList : Parser[Any] = rep(PropExpList)
@@ -15,10 +15,9 @@ class CICOMparser extends RegexParsers{
     def Bool : Parser[Any] = "true" | "false"
     def Unop : Parser[Any] = Sign | "~"
     def Sign : Parser[Any] = "+" | "-"
-    def Binop : Parser[Any] = Sign | "*" | "/" | "=" | "!=" |  "<=" | ">=" |"<" | ">" | "&" | "|"
+    def Binop : Parser[Any] = Sign | "*" | "/" |  "!="| "=" |  "<=" | ">=" |"<" | ">" | "&" | "|"
     def Prim : Parser[Any] = "number?" | "function?" | "list?" |"empty?" | "cons?" | "cons" | "first" | "rest" | "arity"
-    def Id : Parser[Any] = Characters ~ rep(Characters|Digits)
-    def Intg : Parser[Any] = Digits~rep(Digits)
-
-
+    def Id : Parser[Any] = not(resKey) ~> Characters ~ rep(not(resKey)~> Characters|Digits)
+    def Intg : Parser[Any] = rep1(Digits)
+    def resKey :Parser[Any]= "let" | "in" | "map"  |"if" |"then" |"else" |"to"
 }
